0755ddbfe514a93f3c139b18554a1906f43bfb6f
Mahesh <qa.maheshdasari@gmail.com>
2021-02-11 11:12:33 +0530

    11th feb created new class

---
+++ Divya_Workspace/ecommerce/src/main/java/com/durgasoft/ecommerce/basepage/NewClass.java
@@ -0,0 +1,10 @@
+package com.durgasoft.ecommerce.basepage;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is first line in new class");
+
+	}
+
+}
